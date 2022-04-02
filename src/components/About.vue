f2fc590d2b07ebf216529a1dc7552bdba8f7a6b1
Gerardo Rafael Cabrera Herrera <gecabrera@outlook.com>
2022-04-01 22:37:21 -0600

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+    <section id="sobre-mi" class="about-section">
+        <h2>Sobre mi</h2>
+        <div class="row-two-columns">
+            <div class="column">
+                <img src="../assets/img/perfil.png" alt="perfil-portafolio" class="about-img">
+            </div>
+            <div class="column about-text">
+                <p>Mi nombre es Gerardo Cabrera y soy ingenierio en tecnologias computacionales graduado de la Universidad CNCI de Mexico.
+                Soy desarrollador frontend junior de Javascript y desarrollador backend junior en PHP. Manejo frameworks como Laravel en
+                el backend y VueJS en el frontend. Manejo base de datos mediante MYSQL. Hago maquetacion de paginas en HTML y diseñarlas
+                mediante CSS. Tambien utilizo frameworks css como Bootstrap y librerias como JQuery.</p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "About",
+}
+</script>
